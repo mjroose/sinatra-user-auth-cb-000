bf1d61ce3bc8d4b9345a47c0c094c63fa5cb5720
Michael Roose <mjroose@gmail.com>
2018-10-28 20:38:17 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,11 @@ class ApplicationController < Sinatra::Base
 
   get '/users/home' do
     @user = User.find(session[:id])
-    erb :'/users/home'
+    if @user
+      erb :'/users/home'
+    else
+      erb :'/session/login'
+    end
   end
 
 end
